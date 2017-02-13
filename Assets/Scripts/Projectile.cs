14fba0f336eec219a9c25dd539013f180707cd45
josephquested <josephquested@gmail.com>
2017-02-13 15:06:03 +1300

    initial build running

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Bound")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
